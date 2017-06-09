f2f4628127177801f820d8b12dedf0cce5ab1e18
Joe Romero <joethemovie@gmail.com>
2017-06-09 12:38:17 -0400

    initial commit

---
+++ js/master-min.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $(".info1").fadeOut(0, function() {});
+    $(".info2").fadeOut(0, function() {});
+    $(".imgHolder").hover(function() {
+        $(".hoverBut").fadeOut("slow/300/fast", function() {});
+    });
+    $(".hover1").hover(function() {
+        $(".bg").fadeOut("slow/100/fast", function() {});
+        $(".titles").fadeOut("slow/100/fast", function() {});
+        $(".info1").fadeOut("slow/100/fast", function() {});
+        $(".cx5").fadeOut("slow/100/fast", function() {});
+        $(".mx5").fadeIn("slow/100/fast", function() {});
+        $(".info2").fadeIn("slow/100/fast", function() {});
+    });
+    $(".hover2").hover(function() {
+        $(".bg").fadeOut("slow/100/fast", function() {});
+        $(".titles").fadeOut("slow/100/fast", function() {});
+        $(".info2").fadeOut("slow/100/fast", function() {});
+        $(".mx5").fadeOut("slow/100/fast", function() {});
+        $(".cx5").fadeIn("slow/100/fast", function() {});
+        $(".info1").fadeIn("slow/100/fast", function() {});
+    });
+});
